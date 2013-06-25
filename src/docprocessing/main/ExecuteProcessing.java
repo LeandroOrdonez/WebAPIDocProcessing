dcb167090a5f6b2c2dbeda96a1210e77fa4aefb9
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-06-25 14:40:38 +0200

    Update ExecuteProcessing.java

--- src/docprocessing/main/ExecuteProcessing.java
+++ src/docprocessing/main/ExecuteProcessing.java
@@ -5,6 +5,7 @@
 package docprocessing.main;
 
 import docprocessing.rpc.soap.DictGenerator;
+import docprocessing.rpc.soap.TextFilesGenerator;
 import docprocessing.testing.*;
 import docprocessing.rpc.soap.WSDLParsing;
 import docprocessing.rpc.soap.persistence.CrudManager;
@@ -23,16 +24,19 @@ public class ExecuteProcessing {
     public static void main(String[] args) {
         try {
             CrudManager manager = new CrudManager();
-            BufferedReader br = new BufferedReader(new FileReader("src/wsdl-reg/service-uris.txt"));
+//            BufferedReader br = new BufferedReader(new FileReader("src/wsdl-reg/service-uris.txt"));
+            BufferedReader br = new BufferedReader(args[0]!=null ? new FileReader(args[0]) : new FileReader("src/wsdl-reg/service-uris.txt"));
             String wsdlUri;
             int i = 0;
             while ((wsdlUri = br.readLine()) != null) {
                 i++;
                 System.out.print(i + ". ");
+                TextFilesGenerator.DOC_PATH = args[1]!=null ? args[1] : TextFilesGenerator.DOC_PATH; //passing this path as argument to the .jar executable
                 WSDLParsing.parseAndStoreService(new URL(wsdlUri), manager);
                 System.out.println("\n----------------------------------------------------------------------------------------\n");
             }
-            DictGenerator.generateDocDictionary(DictGenerator.DOC_PATH);
+//            DictGenerator.generateDocDictionary(DictGenerator.DOC_PATH);
+            DictGenerator.generateDocDictionary(TextFilesGenerator.DOC_PATH);
         } catch (Exception ex) {
             Logger.getLogger(StorageTest.class.getName()).log(Level.SEVERE, null, ex);
         }
