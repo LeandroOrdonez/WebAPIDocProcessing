82fde511880099e87bc3117b4b50e448f53cf2c8
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-09-24 22:13:05 -0500

    Added Pair.java (Tuple Class) Updated CamelCaseFilter.java: Added getIndexedCamelCaseWords() method

---
+++ src/docprocessing/util/Pair.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package docprocessing.util;
+
+/**
+ *
+ * @author Leandro Ordonez <leandro.ordonez.ante@gmail.com>
+ */
+public class Pair<L, R> {
+
+    private final L left;
+    private final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public int hashCode() {
+        return left.hashCode() ^ right.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Pair pairo = (Pair) o;
+        return this.left.equals(pairo.getLeft())
+                && this.right.equals(pairo.getRight());
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + left.toString() + ", " + right.toString() + ")";
+    }
+}
