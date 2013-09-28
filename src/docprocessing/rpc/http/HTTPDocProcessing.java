e1ca3e683713e8561d28f893e3c419346a96c0db
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2013-09-27 22:42:16 -0500

    Updated HTTPDocProcessing.java: performing the extraction of operation and their description blocks (first approximation).

--- src/docprocessing/rpc/http/HTTPDocProcessing.java
+++ src/docprocessing/rpc/http/HTTPDocProcessing.java
@@ -12,14 +12,21 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.htmlparser.jericho.Attribute;
+import net.htmlparser.jericho.CharacterReference;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.HTMLElements;
 import net.htmlparser.jericho.OutputDocument;
 import net.htmlparser.jericho.Source;
+import net.htmlparser.jericho.StartTag;
 import net.htmlparser.jericho.StartTagType;
 import net.htmlparser.jericho.Util;
 
@@ -29,13 +36,19 @@ import net.htmlparser.jericho.Util;
  */
 public class HTTPDocProcessing {
 
-    // list of HTML attributes that will be retained in the final output:
-//	private static final Set<String> VALID_ATTRIBUTE_NAMES=new HashSet<String>(Arrays.asList(new String[] {
+//     list of HTML attributes that will be retained in the final output:
+	private static final Set<String> VALID_ATTRIBUTE_NAMES=new HashSet<String>(Arrays.asList(new String[] {
 //		"id","class","href","target","title"
-//	}));
+            "title"
+	}));
     public static void main(String[] args) {
+        if (false) {
+            System.getProperties().put("http.proxyHost", "proxy.unicauca.edu.co");
+            System.getProperties().put("http.proxyPort", "3128");
+        }
         String sourceUrlString = "http://www.benchmarkemail.com/API/Library";
-//        String sourceUrlString = "/home/leandro/Escritorio/Email Marketing API - Document Library.html";
+//        String sourceUrlString = "http://www.holidaywebservice.com/ServicesAvailable_HolidayService2.aspx";
+
 //        OutputDocument output = removeElements(sourceUrlString, HTMLElementName.SCRIPT, HTMLElementName.HEAD, HTMLElementName.LINK, HTMLElementName.IMG);
         OutputDocument output = cleanHTML(sourceUrlString);
 //        System.out.println(output.toString());
@@ -54,7 +67,44 @@ public class HTTPDocProcessing {
 //        System.out.println(tagMap);
         String electedTag = getMostPopularTag(tagMap);
         System.out.println("Elected Tag: " + electedTag + " (" + tagMap.get(electedTag).size() + " operations)");
+        List<String> operationList = tagMap.get(electedTag);
+        System.out.println("Lista de Operaciones: " + operationList);
+        try {
+            HashMap<String, String> operationMap = new HashMap<>();
+            for (Element e : cleanedHtml.getAllElements()) {
+                Source elementSource = new Source(e.getContent());
+                elementSource.fullSequentialParse();
+                List<Pair<Integer, String>> indexedCamelCaseWords = CamelCaseFilter.getIndexedCamelCaseWords(e.getContent().toString());
+                List<Pair<Integer, String>> operations = new ArrayList<>();
+                for (Pair<Integer, String> pair : indexedCamelCaseWords) {
+                    if (operationList.contains(pair.getRight())) {
+                        operations.add(pair);
+                    }
+                }
+                int numberOfMatches = 0;
+                Pair<Integer, String> match = null;
+                for (Pair<Integer, String> ccWordPair : operations) {
+//                    Element enclosingElement = elementSource.getEnclosingElement((int) ccWordPair.getLeft());
+                    System.out.println(ccWordPair);
+//                    Element parentElement = enclosingElement.getParentElement();
+                    if (e.getName().equals(electedTag)) {
+                        match = ccWordPair;
+                        numberOfMatches = numberOfMatches += 1;
+                        if (numberOfMatches > 1) {
+                            match = null;
+                            break;
+                        }
+                    }
 
+                }
+                if (match != null) {
+                    operationMap.put(match.getRight(), e.getTextExtractor().toString());
+                }
+            }
+            System.out.println("Mapa de Operaciones: " + operationMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -86,6 +136,18 @@ public class HTTPDocProcessing {
                 outputDocument.remove(source.getAllElements(element));
             }
             outputDocument.remove(source.getAllElements(StartTagType.COMMENT));
+            outputDocument.remove(source.getAllElements(StartTagType.DOCTYPE_DECLARATION));
+            outputDocument.remove(source.getAllElements(StartTagType.XML_DECLARATION));
+            outputDocument.remove(source.getAllElements(StartTagType.CDATA_SECTION));
+            outputDocument.remove(source.getAllElements(StartTagType.SERVER_COMMON));
+            String outputString = outputDocument.toString();
+            source = new Source(outputString);
+            for (StartTag st : source.getAllStartTags()) {
+                String cleanStartTag = getStartTagHTML(st).toString();
+                outputString = outputString.replace(st.toString(), cleanStartTag);
+            }
+            source = new Source(outputString);
+            outputDocument = new OutputDocument(source);
 //            System.out.println(outputDocument.toString());
             return outputDocument;
         } catch (MalformedURLException ex) {
@@ -120,23 +182,25 @@ public class HTTPDocProcessing {
         for (String tag : tagMap.keySet()) {
             if (tagMap.get(tag).size() > maxSize) {
                 electedTag = tag;
+                maxSize = tagMap.get(tag).size();
             }
         }
         return electedTag;
     }
-    
+
     /**
      *
      * @param htmlSource
      * @return
      */
     public static HashMap<String, List<String>> getTagMap(Source htmlSource) {
+        htmlSource.fullSequentialParse();
         List<Pair<Integer, String>> indexedCamelCaseWords = CamelCaseFilter.getIndexedCamelCaseWords(htmlSource.toString());
         HashMap<String, List<String>> tagMap = new HashMap<>();
         for (Pair p : indexedCamelCaseWords) {
             String ccWord = p.getRight().toString();
             Element enclosingElement = htmlSource.getEnclosingElement((int) p.getLeft());
-            Element parentElement = htmlSource.getEnclosingElement(enclosingElement.getBegin() - 1);
+            Element parentElement = enclosingElement.getParentElement();
 //            System.out.println(parentElement.getName() + ", " + ccWord);
             if (isOperation(ccWord)) {
                 if (tagMap.containsKey(parentElement.getName())) {
@@ -152,22 +216,23 @@ public class HTTPDocProcessing {
         }
         return tagMap;
     }
-//    private static CharSequence getStartTagHTML(StartTag startTag) {
-//		// tidies and filters out non-approved attributes
-//		StringBuilder sb=new StringBuilder();
-//		sb.append('<').append(startTag.getName());
-//	  for (Attribute attribute : startTag.getAttributes()) {
-//	    if (VALID_ATTRIBUTE_NAMES.contains(attribute.getKey())) {
-//				sb.append(' ').append(attribute.getName());
-//				if (attribute.getValue()!=null) {
-//					sb.append("=\"");
-//				  sb.append(CharacterReference.encode(attribute.getValue()));
-//					sb.append('"');
-//				}
-//			}
-//	  }
-//	  if (startTag.getElement().getEndTag()==null && !HTMLElements.getEndTagOptionalElementNames().contains(startTag.getName())) sb.append(" /");
-//		sb.append('>');
-//		return sb;
-//	}
+    
+    private static CharSequence getStartTagHTML(StartTag startTag) {
+		// tidies and filters out non-approved attributes
+		StringBuilder sb=new StringBuilder();
+		sb.append('<').append(startTag.getName());
+	  for (Attribute attribute : startTag.getAttributes()) {
+	    if (VALID_ATTRIBUTE_NAMES.contains(attribute.getKey())) {
+				sb.append(' ').append(attribute.getName());
+				if (attribute.getValue()!=null) {
+					sb.append("=\"");
+				  sb.append(CharacterReference.encode(attribute.getValue()));
+					sb.append('"');
+				}
+			}
+	  }
+	  if (startTag.getElement().getEndTag()==null && !HTMLElements.getEndTagOptionalElementNames().contains(startTag.getName())) sb.append(" /");
+		sb.append('>');
+		return sb;
+	}
 }
